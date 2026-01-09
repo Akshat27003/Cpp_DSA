e753233f093c5b7bae84752f0c8095768b2b5146
Akshat27003 <akshat27003@gmail.com>
2026-01-09 23:42:23 +0530

    freopen updated

--- CppBasics/usingFreopen.cpp
+++ CppBasics/usingFreopen.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin); // if you ever try to read input, read it from input.txt file.
+    freopen("output.txt","w",stdout); // whatever i print using cout, send it to output.txt file.
 
-    cout << "Hello!"; // Output: Hello! in output.txt file
+    int a, b;
+    cin >> a >> b; // read the value of a and b from input.txt
+    // suppose 5 3 is written in input.txt. a = 5 and b = 3.
+    // input.txt should be in the SAME DIRECTORY from where the program in being run.
+    cout << a + b; // Output: 5 + 3 = 8 will be stored and reflected in output.txt file
     
     return 0;
 }
